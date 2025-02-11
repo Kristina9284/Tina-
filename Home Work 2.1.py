05d93a359440281f11a0afd68acc6ce5eb1cc92c
kris <kristinatina8448@gmail.com>
2025-02-12 01:16:23 +0200

    HI

---
+++ Home Work 2.1.py
@@ -0,0 +1,8 @@
+user_input = input("Please enter the integer number: ") #2048
+print(user_input)
+int(2048)
+number = int(user_input)
+print(number//2)
+print(number//2//128)
+print(number//2//128*5)
+print(number//2//128*5//2)
