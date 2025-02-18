e36862c193780d78b58d22fff04f00abe4275a59
kris <kristinatina8448@gmail.com>
2025-02-18 02:22:32 +0200

    HI

---
+++ Home Work 3.3.py
@@ -0,0 +1,22 @@
+lst = [1,2,3,4,5,6]
+lst2 = lst[:3]
+lst3 = lst[3:6]
+lst4 = [lst2,lst3]
+print(lst,lst4)
+q = [1,2,3,4,5]
+s = q[:3]
+z = q[3:5]
+h = [s,z]
+print(q,h)
+a = [1,2,3]
+b = a[:2]
+c = a[2:3]
+d = [b,c]
+print(a,d)
+p = [1]
+l = []
+j = [p,l]
+print(p,j)
+o = []
+x = [[]]*2
+print(o,x)
